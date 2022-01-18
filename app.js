5da31d5ce9c3d9c6b4141eb3636f8b7e1dc1ab59
Jacob Cho <jacobcho@live.com>
2022-01-18 11:48:24 +0000

    UI elem in app

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-alert(1)
+/*
+Game:
+- Player guesses number between min & max
+- Player has limited number of guesses
+- Notify Player of guesses remaining
+- Notify Player of the correct answer if game over
+- Play again option
+
+*/
+
+//game values
+let min = 1, 
+    max = 10,
+    winningNum = 2,
+    guessesLeft = 3;
+
+//UI Elements
+const UIgame = document.querySelector('#game'),  // OR document.getElementById('game'),
+      UIminNum = document.querySelector('.min-num'),  //class is . // id is #
+      UImaxNum = document.querySelector('.max-num'),
+      UIguessBtn = document.querySelector('#guess-btn'),
+      UIguessInput = document.querySelector('#guess-input'),
+      UImessage = document.querySelector('.message')
